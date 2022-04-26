8b8219b811df7b9d29a3b639bc6323513c22ae4c
Geniroh <irochibuzor@gmail.com>
2022-04-26 15:14:55 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace wella_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! and Welcome to lessthan100 app, lets help you check if your sum total is less than 100");
+            Console.Write("lessthan100(x, y) Enter your first number: ");
+            double firstNum = double.Parse(Console.ReadLine());
+            Console.Write("lessthan100(" + firstNum + ",y) Enter your second number: ");
+            double secondNum = double.Parse(Console.ReadLine());
+
+            double total = firstNum + secondNum;
+
+            if (total < 100)
+            {
+                Console.WriteLine("lessthan100(" + firstNum + "," + secondNum + ") is true");
+            }
+            else
+            {
+                Console.WriteLine("lessthan100(" + firstNum + "," + secondNum + ") is false");
+            }
+        }
+    }
+}
